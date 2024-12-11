89fac2757bee99ccd453dc879f5bd3f3cf6114a4
AJ Glodowski <ajglodo@gmail.com>
2024-12-11 17:11:07 -0500

    Image source fixes

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'tv-show-app-next-js.vercel.app',
+                hostname: '**.vercel.app',
                 port: '',
                 pathname: '/**/*',
             },
