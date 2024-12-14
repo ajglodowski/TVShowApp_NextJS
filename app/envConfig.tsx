118753372d7eb38f579700e79002d841c768ddc6
AJ Glodowski <ajglodo@gmail.com>
2024-12-14 00:34:22 -0500

    More env variable tweaks

--- app/envConfig.tsx
+++ app/envConfig.tsx
@@ -1,7 +1,7 @@
 export const imageUrlBase = process.env.IMAGE_BASE_URL;
 const prodUrl = 'https://tv-show-app-next-js.vercel.app';
 const localUrl = 'http://localhost:3000';
-const isProd = process.env.VERCEL_ENV === 'production';
+const isProd = process.env.VERCEL_ENV === 'production' || process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
 export const serverBaseURL = isProd ? prodUrl : localUrl;
 export const apiRoute = isProd ? prodUrl : ''; 
-export const clientBaseURL = isProd ? prodUrl : localUrl;
+export const clientBaseURL = isProd ? '' : localUrl;
