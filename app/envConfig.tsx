60964ddfc65a4015f28691da02c6de824a7713a4
AJ Glodowski <ajglodo@gmail.com>
2026-09-22 15:27:21 -0400

    domain name change

--- app/envConfig.tsx
+++ app/envConfig.tsx
@@ -1,4 +1,4 @@
-const prodUrl = 'https://tv-show-app-next-js.vercel.app';
+const prodUrl = 'https://showlog.tv';
 const localUrl = 'http://localhost:3000';
 const isProd = process.env.VERCEL_ENV === 'production' || process.env.NEXT_PUBLIC_VERCEL_ENV === 'production';
 export const serverBaseURL = isProd ? prodUrl : localUrl;
